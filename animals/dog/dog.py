c62f13e7f6935444c549741718c2bfed1c3e0320
KirinLjh <1712240212@qq.com>
2024-03-01 14:01:52 +0800

    add

---
+++ animals/dog/dog.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+import random
+
+# 创建一个窗口
+root = tk.Tk()
+root.title("数字绘制狗")
+
+# 创建一个画布
+canvas = tk.Canvas(root, width=200, height=200)
+canvas.pack()
+
+# 定义一个数字矩阵来表示狗的图像
+dog_image = [
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 0, 0],
+    [0, 1, 0, 1, 1, 0, 1, 0],
+    [1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1],
+    [0, 1, 0, 1, 1, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 1, 1, 1, 1, 0, 0]
+]
+
+# 绘制狗的图像
+cell_size = 20
+for i in range(8):
+    for j in range(8):
+        if dog_image[i][j] == 1:
+            color ='black'
+        else:
+            color = 'white'
+        canvas.create_rectangle(j*cell_size, i*cell_size, (j+1)*cell_size, (i+1)*cell_size, fill=color)
+
+# 运行程序
+root.mainloop()
